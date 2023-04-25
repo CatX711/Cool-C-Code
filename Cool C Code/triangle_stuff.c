ccda27da1d0751e0a3e08a71c4e6c0abefe11d29
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-04-25 21:58:42 +0100

    Add files via upload

---
+++ Cool C Code/triangle_stuff.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double A;
+    double B;
+    double C;
+
+
+    printf("\nFind the hypotenuse of a triangle.");
+    printf("\n\nFirstly, enter side A: ");
+    scanf("%lf", &A);
+
+    printf("\nNow, enter side B: ");
+    scanf("%lf", &B);    
+
+    // calculate hypotenuse of triangle
+    C = sqrt(A*A + B*B); // (a x a + b * b squared)
+
+    printf("\n\nSide C: %lf", C);
+
+    return 0;
+}
